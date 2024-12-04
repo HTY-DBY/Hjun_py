a9b6b9b382193927632847d00d4339186ce52b30
HTY-DBY <hty@hty.ink>
2024-12-04 14:44:56 +0800

    -
    -

--- Other/GobleD.py
+++ Other/GobleD.py
@@ -9,7 +9,7 @@ develop = 1 if get_windows_username_In_Other_main() == '99791' else 0
 class GobleD:
 	def __init__(self, ):
 		self.Main_Path = r"/mnt/d/hty/creat/paper/do/HJun/PY" if develop \
-			else r"/mnt/d/hty/creat/paper/do/HJun/PY"
+			else r"/mnt/c/hty/hj/Hjun_py-main"
 		self.sratoolkit = r'/home/hty/ins/sratoolkit.3.1.1-ubuntu64/bin' if develop \
 			else r'/home/hty/ins/sratoolkit.3.1.1-ubuntu64/bin'
 		self.prefetch = os.path.join(self.sratoolkit, 'prefetch') if develop \
@@ -19,13 +19,13 @@ class GobleD:
 
 		# 数据库主路径
 		self.database_path = r'/mnt/d/hty/creat/paper/do/HJun/Database' if develop \
-			else r'/mnt/d/hty/creat/paper/do/HJun/Database'
+			else r'/mnt/e/Database'
 
 		# 具体的文件
 		self.database_excel = os.path.join(self.database_path, "Database.xlsx") if develop \
 			else os.path.join(self.database_path, "Database.xlsx")
 		self.kraken2_db = r'/mnt/d/hty/creat/paper/do/HJun/Tool/kraken2_db' if develop \
-			else r'/mnt/d/hty/creat/paper/do/HJun/Tool/kraken2_db'
+			else r'/mnt/c/hty/hj/Tool/kraken2_db'
 		# 具体的路径
 		self.excel = os.path.join(self.database_path, "excel") if develop \
 			else os.path.join(self.database_path, "excel")
--- Pretreatment/sra_handle/a1_sra_down.py
+++ Pretreatment/sra_handle/a1_sra_down.py
@@ -1,11 +1,10 @@
-# %%
 import os
+import time
 import subprocess
-
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import pandas as pd
-
 from Other.GobleD import GobleD
-from Other.Other_main import creat_path_In_Other_main
+from Other.Other_main import creat_path_In_Other_main, check_files_exist_and_non_empty_In_Other_main
 
 # 配置参数
 sra_save_path = GobleD().sra_save_path
@@ -22,25 +21,56 @@ database_ori_SRA = pd.read_excel(GobleD().database_excel, sheet_name='SRA')
 test_do = 1
 column_SRA_ID = 'SRA_ID_test' if test_do else 'SRA_ID'
 
-for row in range(len(database_ori_SRA[column_SRA_ID].dropna(how='all'))):
-	sra_id = database_ori_SRA.loc[row, column_SRA_ID]
-	size_mb = database_ori_SRA.loc[row, 'Size (Mb)']
 
+# 函数：下载 SRA 文件
+def download_sra(sra_id, sra_save_path, row, total_rows):
 	sra_file_path = os.path.join(sra_save_path, sra_id, f'{sra_id}.sra')
 
-	# 检查是否已存在
-	if os.path.exists(sra_file_path):
-		print(f'{sra_id} 已经存在')
-		continue
+	# 检查文件是否存在且非空
+	if check_files_exist_and_non_empty_In_Other_main([sra_file_path]):
+		return f'{row + 1}/{total_rows} --- {sra_id} 已经存在'
 
-	# 构建命令
-	# 下载 sra
+	# 构建下载命令
 	cmd_sra_down = f'"{GobleD().prefetch}" "{sra_id}" -O "{sra_save_path}" --max-size u'
-	print(f'开始下载 {sra_id} --- {row + 1}/{len(database_ori_SRA)}')
 
 	try:
+		# 清除可能存在的锁文件
+		lock_file = os.path.join(sra_save_path, sra_id, f'{sra_id}.sra.lock')
+		if os.path.exists(lock_file):
+			os.remove(lock_file)
+			print(f"已删除锁文件: {lock_file}")
+
 		# 执行下载命令
+		print(f"{row + 1}/{total_rows} --- 开始下载 {sra_id}")
 		subprocess.run(cmd_sra_down, shell=True, check=True)
-		print(f'成功下载 {sra_id}')
-	except subprocess.CalledProcessError as e:
-		print(f'下载失败 {sra_id}: {e}')
+		print(f"{row + 1}/{total_rows} --- 成功下载 {sra_id}")
+	except Exception as e:
+		print(f"{row + 1}/{total_rows} --- 下载失败 {sra_id}: \n{e}")
+
+		# 如果下载失败，重试机制
+		retry_count = 3
+		while retry_count > 0:
+			print(f"重试中: {row + 1}/{total_rows} --- {retry_count} 尝试下载 {sra_id}")
+			try:
+				subprocess.run(cmd_sra_down, shell=True, check=True)
+				print(f"{row + 1}/{total_rows} --- 成功下载 {sra_id}")
+				break
+			except Exception as e:
+				print(f"{row + 1}/{total_rows} --- 未知错误 {sra_id}: \n{e}")
+
+			retry_count -= 1
+			time.sleep(10)  # 等待后重试
+	print('---------------------------------------')
+
+
+# 多线程下载
+max_threads = os.cpu_count()  # 设置线程数量
+count_database_ori_SRA = len(database_ori_SRA[column_SRA_ID].dropna(how='all'))
+with ThreadPoolExecutor(max_threads) as executor:
+	futures = {
+		executor.submit(download_sra, database_ori_SRA.loc[row, column_SRA_ID], sra_save_path, row, count_database_ori_SRA): row
+		for row in range(count_database_ori_SRA)
+	}
+
+	for future in as_completed(futures):
+		print(future.result())
